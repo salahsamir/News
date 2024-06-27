f495455daf7c1e7c64e609f42eb84605bef4b993
salah samir <salahsaamir707@gmail.com>
2024-06-27 20:51:31 +0300

    finish

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,16 +1,16 @@
 import Currency from "../component/Currency/Currency";
 import Gold from "../component/Gold";
-
+import News from "../component/News"
 export default function Index() {
   return (
     <>
     <div>
       {/* News Section    */}
-      {/* <section>
+      <section>
         <h2 className="text-4xl font-bold text-center my-2 py-2"> <span className="text-blue-700">L</span>atset <span className="text-blue-700">N</span>ews</h2>
         <News/>
 
-      </section> */}
+      </section>
 
       {/* Currency Section */}
       <section>
