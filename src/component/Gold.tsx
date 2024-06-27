ac2d61005ed2d2c6c55c193377f08dd760ce88ff
salah samir <salahsaamir707@gmail.com>
2024-06-27 12:14:15 +0300

    clean Gold  code

--- src/component/Gold.tsx
+++ src/component/Gold.tsx
@@ -6,46 +6,41 @@ import { Loading } from "../ui/Loading";
 
 
 const Gold = () => {
-  const { data, isLoading, error } = useFetchGoldPricesQuery();
-  
+  const { data, isLoading } = useFetchGoldPricesQuery();
 
-  if (isLoading) return <Loading/>;
+  if (isLoading) return <Loading />;
 
-  const chartData = [
-    data.price_gram_24k,
-    data.price_gram_22k,
-    data.price_gram_21k,
-    data.price_gram_18k,
-    data.price_gram_16k,
-    data.price_gram_14k,
-    data.price_gram_10k,
-  ];
-
-  let categories= ["24K", "22K", "21K", "18K", "16K", "14K", "10K"]
-  
+  const goldPrices = {
+    '24K': data.price_gram_24k,
+    '22K': data.price_gram_22k,
+    '21K': data.price_gram_21k,
+    '18K': data.price_gram_18k,
+    '16K': data.price_gram_16k,
+    '14K': data.price_gram_14k,
+    '10K': data.price_gram_10k,
+  };
 
   return (
-
-  <>
     <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-3">
-
-  <ChartsUi chartData={chartData} categories={categories} name=' Gold Prices' title=" EGP per Gram" />
-    <div className="m-5 shadow-lg p-5 ">
-      {categories.map((category, index) => (
-        <div className="flex gap-4  justify-around">
-          <h4 key={index} className="text-5xl font-semibold py-3  ">{category}:</h4>
-          <h4 className="text-5xl font-semibold py-3 text-blue-700">{chartData[index].toFixed(2)} <span className="text-black"> EGP</span></h4>
-          <b/>
-        </div>
-
-        
-
-      ))}
+      <ChartsUi
+        chartData={Object.values(goldPrices)}
+        categories={Object.keys(goldPrices)}
+        name="Gold Prices"
+        title="EGP per Gram"
+      />
+      <div className="m-5 shadow-lg p-5">
+        {Object.entries(goldPrices).map(([category, price]) => (
+          <div className="flex gap-4 justify-around" key={category}>
+            <h4 className="text-5xl font-semibold py-3">
+              {category}:
+            </h4>
+            <h4 className="text-5xl font-semibold py-3 text-blue-700">
+              {price.toFixed(2)} <span className="text-black">EGP</span>
+            </h4>
+          </div>
+        ))}
+      </div>
     </div>
-
-    </div>
-
-  </>
   );
 };
 
